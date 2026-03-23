04040d9fa9b686170fda28e6d8a4b6ad4767361f
King_Jace <kingj2@rhodes.edu>
2026-03-23 11:52:13 -0500

    Day 24 updates

--- notes/day24tokenizers/expander.rkt
+++
@@ -1,7 +0,0 @@
-#lang br/quicklang
-
-; expander stub: it does nothing but display the produced module datum
-(define-macro (bf-module-begin PARSE-TREE)
-  #'(#%module-begin
-     'PARSE-TREE))
-(provide (rename-out [bf-module-begin #%module-begin]))
